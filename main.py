b922cdccc1448aabf265cde4e23bce70631c4387
Neil Humphrey <humphnei000@edmonds15.org>
2022-04-11 11:58:42 -0700

    Added code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+class Stream:
+    def __init__(self,data):
+        self.data = list(data)
+        self.inx = 0
+    def at(self,i=0):
+        return self.data[self.inx + i]
+    def inc(self,i=1):
+        self.inx += i
+
+class ExpStatement:
+    def __init__(self,exp):
+        self.exp = exp
+def readStatement(strm):
+    pass
+
+with open("src.txt") as file:
+    data = file.read()
+    print(data)
+    strm = Stream(data)
